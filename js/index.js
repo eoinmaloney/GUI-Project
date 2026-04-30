8d17f144c88f5feba8b669f9af3751c5b06d166f
eoinmaloney <g00425150@atu.ie>
2026-04-30 20:33:01 +0100

    Created the post button (Unfunctional)
    HTML
    - Added a new post button
    - Added a new section username for sign up
    
    CSS
    - Styled the text box for posting
    
    JS
    - Created the hide typing container box function

--- js/index.js
+++ js/index.js
@@ -1,19 +1,27 @@
 
 var typingContainer = document.getElementById("typing-container")
 var createPostButton = document.getElementById("create-post-button")
+var closePostButton = document.getElementById("close-button")
 
 function showTypingContainer(){
     typingContainer.style.visibility = "visible"
 }
 
-createPostButton.addEventListener("click", showTypingContainer)
+function hideTypingContainer(){
+    typingContainer.style.visibility = "hidden"
+}
 
+createPostButton.addEventListener("click", showTypingContainer)
+closePostButton.addEventListener("click", hideTypingContainer)
 /*      Sources
 - 
 */
 
 /*      To do
 -   Check if user has an account
--   Add an X button to top right of create post that closes the menu
 -   Add a whole div when the user clicks post
+*/
+
+/*      Completed
+-   Add an X button to top right of create post that closes the menu 
 */
---
+++ js/profile.js
@@ -0,0 +1,4 @@
+/*      To do
+- Check if user has an account
+- Add an image input system to change profile picture
+*/
