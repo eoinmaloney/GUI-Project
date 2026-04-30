20d0ff166bc9b10cb96f5f7fe6755d5c23457e15
eoinmaloney <g00425150@atu.ie>
2026-04-30 21:11:32 +0100

    Redirect users if they don't have an account to sign up
    HTML
    - Changed structure of profile details to fit JS
    
    JS
    - Added functionality to display user details if in local storage
    - Added username input on sign up

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,26 @@
+var email = localStorage.getItem("signup_email")
+var username = localStorage.getItem("signup_username")
+var password = localStorage.getItem("signup_password")
+
+var email_label = document.getElementById("email-label")
+var username_label = document.getElementById("username-label")
+var password_label = document.getElementById("password-label")
+
+if(email === null || username === null || password === null){
+    window.location.href = "../root/sign-up.html"
+}
+else {
+    email_label.innerHTML = "Email: " + email
+    username_label.innerHTML = "Username: " + username
+    password_label.innerHTML = "Password: " + password
+}
+
 /*      To do
 - Check if user has an account
 - Add an image input system to change profile picture
+*/
+
+/*      Completed
+- Redirect user if they don't have an account
+- Display user details on page
 */
--- js/sign-up.js
+++ js/sign-up.js
@@ -1,12 +1,16 @@
 function saveSignup() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
+    var username = document.getElementById("username").value;
 
-    if (email === "" || password === "") {
+    if (email === "" || password === "" || username === "") {
         document.getElementById("message").innerText = "Please fill in the required fields.";
         return;
     }
-
-    localStorage.setItem("signup_email", email);
-    localStorage.setItem("signup_password", password);
+    else{
+        localStorage.setItem("signup_email", email);
+        localStorage.setItem("signup_password", password);
+        localStorage.setItem("signup_username", username);
+    }
+    
 }
